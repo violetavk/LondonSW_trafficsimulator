70f8a516a953ee6380ce3c6b680938b08073c078
rawanmoh <rawan.rahili@gmail.com>
2016-02-10 18:05:39 +0000

    updates car class

--- src/londonsw/model/simulation/components/vehicles/Car.java
+++ src/londonsw/model/simulation/components/vehicles/Car.java
@@ -5,6 +5,18 @@ package londonsw.model.simulation.components.vehicles;
  * This moves in a lane
  * Only moves forwards when the slot in front of it is empty
  */
+
+// Add Car class layout
 public class Car implements Vehicle {
+
+    int location;
+    int direction;
+
+public void setLocation (int location){};
+public int getlocation (){ return location; };
+
+    public void moveforward () {};
+    public void turn () {};
+
 
 }
