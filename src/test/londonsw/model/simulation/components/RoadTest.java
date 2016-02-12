d6c0c1d64d8adc26db1cbe2a0550ff68f06cf226
Violet <violetavkhukova@gmail.com>
2016-02-12 14:08:45 +0000

    Forgot to add libraries for junit tests yesterday; added potential controller for View; added another method to Lane; started RoadTest class

---
+++ src/test/londonsw/model/simulation/components/RoadTest.java
@@ -0,0 +1,31 @@
+package londonsw.model.simulation.components;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by violet on 12/02/2016.
+ */
+public class RoadTest {
+
+    @Test
+    public void testGetLanes() throws Exception {
+
+    }
+
+    @Test
+    public void testGetStartLocation() throws Exception {
+
+    }
+
+    @Test
+    public void testGetEndLocation() throws Exception {
+
+    }
+
+    @Test
+    public void testGetNumberLanes() throws Exception {
+
+    }
+}
