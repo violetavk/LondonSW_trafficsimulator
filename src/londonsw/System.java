bebb833eb93598a1ab71ffc28c1ba78983cef01c
Violet <violetavkhukova@gmail.com>
2016-02-16 11:26:17 +0000

    Adding some basic GUI screens

--- src/londonsw/System.java
+++ src/londonsw/System.java
@@ -1,12 +1,15 @@
 package londonsw;
 
+import londonsw.controller.StartUpController;
+
 /**
  * This is the main class that first gets executed by the system
  * It starts the View components (and from there will branch to either simulation or map-making)
  */
 public class System {
-	
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws Exception {
+		StartUpController sc = new StartUpController();
+		sc.startSoftware(args);
 	}
+
 }
---
+++ src/londonsw/controller/StartUpController.java
@@ -0,0 +1,56 @@
+package londonsw.controller;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class StartUpController extends Application{
+
+    public Label londonSWlabel;
+    public Label trafficSimLabel;
+    public Button startButton;
+
+    public void startSoftware(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("../view/startup/StartScreen.fxml"));
+        stage.setTitle("LondonSW Traffic Simulator");
+        stage.setScene(new Scene(root));
+        stage.setResizable(false);
+        stage.show();
+    }
+
+
+    public void goToChooseModeScreen(ActionEvent actionEvent) throws IOException {
+        Parent chooseModeScreen = FXMLLoader.load(getClass().getResource("../view/startup/ChooseModeScreen.fxml"));
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(chooseModeScreen));
+    }
+
+    public void goToSimulationMode(ActionEvent actionEvent) {
+        // TODO This is just dummy code to take place of the next screens for now
+//        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+//        alert.setTitle("Simulation Mode");
+//        alert.setHeaderText(null);
+//        alert.setContentText("This is a placeholder for the next screens. The user would load a map and simulate on it.");
+//        alert.showAndWait();
+    }
+
+    public void goToMapBuilderMode(ActionEvent actionEvent) {
+
+
+    }
+}
