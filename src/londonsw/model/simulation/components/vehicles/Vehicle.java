c898b181f55ea4956c7fd686d275a095459276f0
rawanmoh <rawan.rahili@gmail.com>
2016-02-17 10:53:00 +0000

    create vehicle Test modify Vehicle class

--- src/londonsw/model/simulation/components/vehicles/Vehicle.java
+++ src/londonsw/model/simulation/components/vehicles/Vehicle.java
@@ -10,7 +10,7 @@ import java.util.List;
  * This is the interface that all vehicles will implement
  * This allows for scalability because we can add more types of cars (eg. ambulance, bus)
  */
-public abstract class Vehicle {
+public  class Vehicle {
 
     public Coordinate vehicleLocation;
     int vehicleId;
