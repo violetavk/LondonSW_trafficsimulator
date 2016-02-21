f01a01eb024b53232e8fd6828573ab5bfb2b79ff
4ND4 <santiago.anda@outlook.com>
2016-02-21 14:40:52 +0000

    Add class MapGridGUI

---
+++ src/londonsw/view/MapGridGUITestMain.java
@@ -0,0 +1,43 @@
+package londonsw.view;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import londonsw.model.simulation.MapGrid;
+import londonsw.model.simulation.components.*;
+import londonsw.view.simulation.MapGridGUI;
+
+public class MapGridGUITestMain extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        MapGridGUI m = new MapGridGUI(10,10);
+
+        Road R1 = new Road(new Coordinate(0,0),new Coordinate(9,0));
+        Lane L1 = new Lane(new Coordinate(0,0), new Coordinate(9,0), MapDirection.EAST);
+        //Intersection I1 = new Intersection(new Coordinate(0,4));
+
+        //Road R2 = new Road(new Coordinate(5,0),new Coordinate(5,9));
+        //Lane L2 = new Lane(new Coordinate(5,0),new Coordinate(5,9),MapDirection.SOUTH);
+
+        R1.addLane(L1);
+        //R2.addLane(L2);
+
+        m.addComponent(R1);
+        //m.addComponent(I1);
+        //m.addComponent(R2);
+
+        m.drawComponents(primaryStage);
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
