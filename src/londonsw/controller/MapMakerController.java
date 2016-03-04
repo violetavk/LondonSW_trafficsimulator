1e0e2aab19630e1ea8d66bedb5cef8642176c180
jia <jia.liu@kcl.ac.uk>
2016-03-04 17:43:42 +0000

    pre-made map

--- src/londonsw/controller/MapMakerController.java
+++ src/londonsw/controller/MapMakerController.java
@@ -7,15 +7,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-//import javafx.scene.input.DataFormat;
-//import javafx.scene.input.Dragboard;
-//import javafx.scene.input.TransferMode;
 import javafx.stage.Stage;
-//import javafx.scene.input.DragEvent;
 
 
 import java.io.IOException;
-//import java.util.Map;
+
 
 
 public class MapMakerController extends Application{
@@ -26,7 +22,7 @@ public class MapMakerController extends Application{
 
     }
 
-
+/**
     public void goToMapCreationScreen(ActionEvent actionEvent) throws IOException {
 
         Parent mapCreation = FXMLLoader.load(getClass().getResource("../view/mapcreation/MapCreation" + ".fxml"));
@@ -34,18 +30,6 @@ public class MapMakerController extends Application{
         Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
 
         stage.setScene(new Scene(mapCreation));
-    }
-
-/**
-    public void setOnDragDetected(ActionEvent actionEvent)throws IOException{
-        Parent mapCreation = FXMLLoader.load(getClass().getResource("../view/mapcreation/MapCreation" + ".fxml"));
-        Node node = mapCreation.lookup("#grass");
-        Dragboard db=node.startDragAndDrop(TransferMode.ANY);
-        ClipboardContent content = new ClipboardContent();
-        content.putString(source.getText());
-        db.setContent((Map<DataFormat, Object>) node);
-        actionEvent.consume();
-
     }
  **/
 
---
+++ src/test/londonsw/controller/MapMakerControllerTest.java
@@ -0,0 +1,16 @@
+package londonsw.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by LiuJia on 3/3/16.
+ */
+public class MapMakerControllerTest {
+
+    @Test
+    public void testGoToMapCreationScreen() throws Exception {
+
+    }
+}
