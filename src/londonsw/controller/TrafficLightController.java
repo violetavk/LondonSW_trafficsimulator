b3d9a2b2076f2cdd1c4771e1d1739d80e120b9c7
Violet <violetavkhukova@gmail.com>
2016-03-05 00:17:16 +0000

    Integrated TrafficLight and Intersection with new Ticker, and fully functioning GUIs for each

--- src/londonsw/controller/TrafficLightController.java
+++ src/londonsw/controller/TrafficLightController.java
@@ -8,48 +8,52 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Controls all the traffic lights. This gets notified when the traffic light changes in the model, and it
+ * does the necessary work to change the GUI to display the new model.
+ */
 public class TrafficLightController {
 
-    private static Map<TrafficLight, TrafficLightDecorator> trafficLights;
-    private static TrafficLightController instance;
-    private static ArrayList<TrafficLight> allLights;
+    private static Map<TrafficLight, TrafficLightDecorator> trafficLights = new HashMap<TrafficLight, TrafficLightDecorator>();;
+    private static ArrayList<TrafficLight> allLights = new ArrayList<TrafficLight>();
 
-    protected TrafficLightController() {
-        trafficLights = new HashMap<TrafficLight, TrafficLightDecorator>();
-        allLights = new ArrayList<TrafficLight>();
-    }
-
-    public static TrafficLightController getInstance() {
-        if(instance == null) {
-            instance = new TrafficLightController();
-        }
-        return instance;
-    }
-
+    /**
+     * Called by the TrafficLight (in the model) when the color changes. It tells the corresponding
+     * TrafficLightDecorator to change its color in the GUI.
+     * @param colour the new color to be
+     * @param tl a TrafficLight that had its color changed
+     */
     public static void colourChanged(LightColour colour, TrafficLight tl) {
-        // tell the view to be the color "colour"
-        System.out.println("TrafficLight " + tl +  " wants to be changed to " + colour);
         if(trafficLights.get(tl) == null) {
             System.out.println("No TrafficLightDecorator associated with this traffic light");
         }
         trafficLights.get(tl).setGUIColour(colour);
     }
 
-    public static void addKeyValuePair(TrafficLight tl, TrafficLightDecorator gui) {
+    /**
+     * Register a TrafficLight (in the model) to a TrafficLightDecorator (in the GUI), and add to an
+     * ArrayList to keep track of all lights
+     * @param tl the TrafficLight instance from the model
+     * @param gui the corresponding instance of the TrafficLightDecorator for that TrafficLight
+     */
+    public static void addTrafficLightAndDecoratorPair(TrafficLight tl, TrafficLightDecorator gui) {
         trafficLights.put(tl,gui);
         allLights.add(tl);
     }
 
+    /**
+     * If you need to get the decorator for that TrafficLight outside of this class, use this method
+     * @param tl a TrafficLight that you are querying for to get its decorator
+     * @return the corresponding TrafficLightDecorator for that TrafficLight
+     */
     public static TrafficLightDecorator getTrafficLightGUI(TrafficLight tl) {
         return trafficLights.get(tl);
     }
 
-    public static TrafficLightDecorator createNewTrafficLightGUI(TrafficLight tl) {
-        TrafficLightDecorator gui = new TrafficLightDecorator(tl);
-//        addKeyValuePair(tl,gui);
-        return gui;
-    }
-
+    /**
+     * Get a list of all the TrafficLights in the system
+     * @return ArrayList of all lights
+     */
     public static ArrayList<TrafficLight> getAllTrafficLights() {
         return allLights;
     }
--- src/londonsw/view/simulation/IntersectionDecorator.java
+++ src/londonsw/view/simulation/IntersectionDecorator.java
@@ -1,20 +1,25 @@
 package londonsw.view.simulation;
 
-import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
+import londonsw.controller.IntersectionController;
 import londonsw.model.simulation.components.Intersection;
 
+/**
+ * Associates an Intersection GUI with an instance of an Intersection. Each has up to 4
+ * TrafficLightDecorators that live inside this GUI. The Intersection does not have any
+ * explicit circles itself, it relies on the circles of each TrafficLightDecorator that
+ * is associated with each TrafficLight in the Intersection instance.
+ *
+ * Exactly one IntersectionDecorator is created for exactly one Intersection.
+ */
 public class IntersectionDecorator {
 
     private Intersection intersection;
-    private int width;
-    private int height;
+    private int width = 100;
+    private int height = 100;
 
     private TrafficLightDecorator northLight;
     private TrafficLightDecorator eastLight;
@@ -23,46 +28,47 @@ public class IntersectionDecorator {
 
     private Group root;
     private Scene scene;
-    private Circle northCircle;
-    private Circle eastCircle;
-    private Circle southCircle;
-    private Circle westCircle;
 
+    /**
+     * Creates a GUI for that specific intersection. It creates TrafficLightDecorators for all
+     * TrafficLights that are in this intersection. It draws and displays them in a Scene specified
+     * by the global variables of width and height.
+     * TODO add a scale factor to the graphics, because each x-coordinate, y-coordinate, and radius is based on 100x100
+     * @param intersection an instance of an Intersection to associate this decorator
+     */
     public IntersectionDecorator(Intersection intersection) {
         this.intersection = intersection;
+        IntersectionController.addIntersectionAndDecoratorPair(intersection,this);
         root = new Group();
-        scene = new Scene(root, 100, 100, Color.BEIGE);
-        StackPane sp = new StackPane();
-        GridPane gridPane = new GridPane();
-        gridPane.setPrefSize(100,100);
-        gridPane.setAlignment(Pos.CENTER);
+        scene = new Scene(root, width, height, Color.BEIGE);
+
         if(intersection.getNorthTrafficLight() != null) {
             northLight = new TrafficLightDecorator(intersection.getNorthTrafficLight());
-            northCircle = northLight.drawLight(50,15,10);
-//            root.getChildren().add(northCircle);
-            gridPane.add(northCircle,1,0);
+            northLight.drawLight(50,15,12);
+            root.getChildren().add(northLight.getCircle());
         }
         if(intersection.getEastTrafficLight() != null) {
             eastLight = new TrafficLightDecorator(intersection.getEastTrafficLight());
-            eastCircle = eastLight.drawLight(15,50,10);
-//            root.getChildren().add(eastCircle);
-            gridPane.add(eastCircle,2,1);
+            eastLight.drawLight(85,50,12);
+            root.getChildren().add(eastLight.getCircle());
         }
         if(intersection.getSouthTrafficLight() != null) {
             southLight = new TrafficLightDecorator(intersection.getSouthTrafficLight());
-            southCircle = southLight.drawLight(50,85,10);
-//            root.getChildren().add(southCircle);
-            gridPane.add(southCircle,1,2);
+            southLight.drawLight(50,85,12);
+            root.getChildren().add(southLight.getCircle());
         }
         if(intersection.getWestTrafficLight() != null) {
             westLight = new TrafficLightDecorator(intersection.getWestTrafficLight());
-            westCircle = westLight.drawLight(85,50,10);
-//            root.getChildren().add(westCircle);
-            gridPane.add(westCircle,0,1);
+            westLight.drawLight(15,50,12);
+            root.getChildren().add(westLight.getCircle());
         }
-        root.getChildren().add(gridPane);
     }
 
+    /**
+     * This is mostly for debug, but it displays the intersection in the Stage provided. This might be able
+     * to extend in the Map
+     * @param stage the Stage to display this Intersection in
+     */
     public void showIntersection(Stage stage) {
         stage.setScene(scene);
         stage.show();
